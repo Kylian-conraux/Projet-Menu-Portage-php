be68be598d00f89576a86ad0938a1589389f4dcf
kylian-conraux <kylianconraux@hotmail.fr>
2024-08-23 13:38:58 +0200

    commentaires

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ switch ($page) {
     case 'home':
         include 'structures/front/pages/home.php';
         break;
-
+        /*Aides financières*/
     case 'aides-financieres':
         include 'structures/front/pages/aides-financieres.php';
         break;
@@ -17,16 +17,16 @@ switch ($page) {
     case 'contact':
         include 'structures/front/pages/contact.php';
         break;
-
+        /*équipe*/
     case 'equipe':
         include 'structures/front/pages/equipe.php';
         break;
 
-
+        /*Les menus*/
     case 'menus':
         include 'structures/front/pages/menus.php';
         break;
-
+        /*Modalités*/
     case 'modalites':
         include 'structures/front/pages/modalites.php';
         break;
