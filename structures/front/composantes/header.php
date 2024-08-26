b58de8cd40aa6bec1ad30f918975bb02c31fe052
kylian-conraux <kylianconraux@hotmail.fr>
2024-08-26 15:44:29 +0200

    changement d'affichage du header selon la page (dynamique)

--- structures/front/composantes/header.php
+++ structures/front/composantes/header.php
@@ -3,9 +3,41 @@
 	<div class="container">
 
 		<!-- Logo -->
-		<img src="assets/css/images/logo_final.svg" alt="" />
-		<p>Service Traiteur et livraison</p>
+		<?php if ($_SERVER['REQUEST_URI'] == '/' || $_SERVER['REQUEST_URI'] == '/index.php'): ?>
+            <img src="assets/css/images/logo_final.svg" alt="Logo" />
+        <?php endif; ?>
 
+		 <!-- Dynamic Text -->
+		  <br>
+		 <h2>
+            <?php 
+                $current_page = basename($_SERVER['REQUEST_URI'], ".php");
+                switch ($current_page) {
+                    case '':
+                    case 'index':
+                        echo "Service Traiteur et livraison";
+                        break;
+                    case 'menus':
+                        echo "Découvrez nos menus savoureux";
+                        break;
+                    case 'aides-financieres':
+                        echo "Profitez de nos aides financières";
+                        break;
+                    case 'modalites':
+                        echo "Découvrez nos modalités de livraison";
+                        break;
+                    case 'equipe':
+                        echo "Rencontrez notre équipe dédiée";
+                        break;
+                    case 'contact':
+                        echo "Contactez-nous pour plus d'informations";
+                        break;
+                    default:
+                        echo "Service Traiteur et livraison";
+                        break;
+                }
+            ?>
+        </h2>
 		<!-- Nav -->
 		<nav id="nav">
 			<ul>
